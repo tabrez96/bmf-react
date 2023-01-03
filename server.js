34ac61e02f91a686976afc8f4de02ceda1e1f560
Tabrez Basha <abtabres@gmail.com>
2023-01-03 21:24:56 +0530

    add config to connect to mongoDB

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+/** connect database */
+connectDB();
+
 const { PORT = 6000 } = process.env;
+
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API running'));
 

31c1d5c315682fb8f09ca15dba8b9e3ab7dca58a
Tabrez Basha <abtabres@gmail.com>
2023-01-03 22:28:40 +0530

    add routes to perform CRUD operations on user

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
 
 const config = require('config');
+const auth = require('../../middleware/auth');
 
 const router = express.Router();
 
@@ -81,5 +82,94 @@ router.post(
     }
   }
 );
+
+/**
+ * @route GET api/users
+ * @desc Get all users
+ * @access private
+ */
+router.get('/', auth, async (req, res) => {
+  try {
+    const users = await User.find().select('-password');
+    res.json(users);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+});
+
+/**
+ * @route GET api/users/:userId
+ * @desc Get info of a specific user
+ * @access private
+ */
+router.get('/:userId', auth, async (req, res) => {
+  try {
+    const user = await User.findOne({ id: req.params.userId }).select(
+      '-password'
+    );
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+});
+
+/**
+ * @route PUT api/users/:userId
+ * @desc Update info of a specific user
+ * @access private
+ */
+router.put(
+  '/:userId',
+  [
+    check('name', 'Name is required').notEmpty(),
+    check('phone', 'Phone number is required').isMobilePhone('en-IN'),
+  ],
+  auth,
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+        });
+      }
+
+      const { name, phone } = req.body;
+
+      const user = await User.findOne({ id: req.params.userId }).select(
+        '-password'
+      );
+
+      user.name = name;
+      user.phone = phone;
+      user.save();
+      res.json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+/**
+ * @route DELETE api/users
+ * @desc delete users by ids
+ * @access private
+ */
+router.delete('/', auth, async (req, res) => {
+  try {
+    const { ids } = req.body;
+
+    const { deletedCount } = await User.deleteMany({ _id: { $in: ids } });
+
+    res.json({ msg: `Deleted ${deletedCount} users` });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
